b300c3b3d14539800bb17aa4c02a5b9a2f28b113
Yogesh <yogesh.r2793@gmail.com>
2025-09-30 01:36:41 +0530

    Updated logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import logo1 from "../../assets/logo1.png";
-import logo2 from "../../assets/logo2.png";
+// import logo2 from "../../assets/logo2.png";
 
 
 import Container from "../shared/container";
